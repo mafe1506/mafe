bdbd01014899d0ed58a2070b3d5ae0cabcef0f15
mafe1506 <161093120+mafe1506@users.noreply.github.com>
2024-02-23 19:51:22 -0500

    Add files via upload

---
+++ Repaso.py
@@ -0,0 +1,32 @@
+class persona:
+    pass
+pedro=persona()
+cristina=persona()
+manuel=persona()
+maria=persona()
+
+pedro.nombre="pedro"
+pedro.edad=34
+pedro.sexo="masculino"
+pedro.pais="colombia"
+
+cristina.nombre="cristina"
+cristina.edad=23
+cristina.sexo="femenino"
+cristina.pais="chile"
+
+manuel.nombre="manuel"
+manuel.edad=28
+manuel.sexo="masculino"
+manuel.pais="argentina"
+
+maria.nombre="maria"
+maria.edad=30
+maria.sexo="femenino"
+maria.pais="brazil"
+
+print(pedro.pais)
+print(cristina.edad)
+print(manuel.sexo)
+print(maria.nombre)
+
